89ed3dcac4f82b6b9f1de19a0c63b4f300192dcb
云龙 <zylcold@163.com>
2020-05-01 23:28:54 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Hello",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    products: [
+        .library(name: "Hello", targets: ["Hello"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
